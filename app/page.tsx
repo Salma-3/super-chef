6aece86386102f5756a9060e3e9e6f1d905ffd35
Salma-3 <salmehdhar@icloud.com>
2024-09-07 21:37:09 +0300

    modify home

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,128 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="">
-      <div className="hero relative">
-        <div className="absolute z-10 top-[50%] left-[50%] ">
-          <form action="">
-            <input type="search" placeholder='Pizza i.e.' className='bg-white'/>
+      <section id='hero' className="hero relative">
+        {/* <div className="search-wrap absolute z-10 left-[10%] right-[10%] top-[50%] md:left-[30%] md:right-unset md:w-[400px] rounded-md">
+          <form className='flex gap-2 shadow'>
+            <input type="search" placeholder='Pizza i.e.' className='w-10/12 p-1 px-3 md:w-11/12 rounded-md focus:outline-none' />
+            <button type='submit' className='p-1 w-3/12 rounded-md text-center center bg-primary text-white'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 mx-auto">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              </svg>
+            </button>
           </form>
         </div>
-      </div>
+        <div className="search-wrap-layer"></div> */}
+      </section>
+      <section id='pro' className='p-6'>
+        <div className='flex w-100 gap-4 md:px-12'>
+          <h2 className='text-primary text-3xl font-bold'>Explore</h2>
+          <div className='grow'>
+            <form className='flex gap-2'>
+              <input type="search" placeholder='Pizza i.e.' className='w-10/12 p-1 px-3 md:w-11/12 rounded-xl border border-gray-400 shadow-lg focus:outline-none' />
+              <button type='submit' className='p-1 w-3/12 rounded-xl bg-primary text-white'>
+                search  
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="flex flex-col justify-around items-center mt-10 xl:justify-center xl:gap-20 md:flex-row">
+          {/* food types: trends, healthy, quick, vegan, lunch box */}
+          <div className="grid grid-cols-2 gap-5 md:w-7/12 lg:w-6/12 lg:grid-cols-3 xl:w-5/12 mb-6">
+            <div className="">
+              <a href="#">
+              <h4 className='text-primary text-3xl'>Trends</h4>
+               <div className="relative w-[150px] h-[150px]">
+                <Image className='rounded-lg' src='/images/trends.webp' fill alt='trends'/>
+               </div>
+              </a>
+            </div>
+            <div className="">
+              <a href="#">
+              <h4 className='text-primary text-3xl'>Quick</h4>
+               <div className="relative w-[150px] h-[150px]">
+                <Image className='rounded-lg' src='/images/quick.jpeg' fill alt='quick'/>
+               </div>
+              </a>
+            </div>
+            <div className="">
+              <a href="#">
+              <h4 className='text-primary text-3xl'>Healthy</h4>
+               <div className="relative w-[150px] h-[150px]">
+                <Image className='rounded-lg' src='/images/healthy.jpg' fill alt='healthy'/>
+               </div>
+              </a>
+            </div>
+            <div className="">
+              <a href="#">
+              <h4 className='text-primary text-3xl'>Vegan</h4>
+               <div className="relative w-[150px] h-[150px]">
+                <Image className='rounded-lg' src='/images/vegan.jpg' fill alt='vegan'/>
+               </div>
+              </a>
+            </div>
+            <div className="">
+              <a href="#">
+              <h4 className='text-primary text-3xl'>Delicious</h4>
+               <div className="relative w-[150px] h-[150px]">
+                <Image className='rounded-lg' src='/images/delicious.jpg' fill alt='delicious'/>
+               </div>
+              </a>
+            </div>
+            <div className="">
+              <a href="#">
+              <h4 className='text-primary text-3xl'>Lunch Box</h4>
+               <div className="relative w-[150px] h-[150px]">
+                <Image className='rounded-lg' src='/images/lunch-box.jpeg' fill alt='lunch box'/>
+               </div>
+              </a>
+            </div>
+          </div>
+          {/* food news */}
+          <div className="news md:w-6/12 lg:w-4/12">
+            <div className="w-100 py-4 px-6 bg-gray-200">
+              <h3 className='text-2xl font-bold text-center'>Food News</h3>
+              <ul className='mt-3'>
+                <li className='mb-6'>
+                  <div className='flex gap-5'>
+                    <Image className='block' src='/images/news-1.webp' width={100} height={80} alt='news'/>
+                    <div>
+                      <h5 className='text-xl font-bold hover:underline'>
+                        <a href="#">Aldi’s Holiday Beer and Wine Selection Has Landed..</a>
+                      </h5>
+                      <p className='text-gray-600'>12 mart 2024</p>
+                    </div>
+                  </div>
+                </li>
+                <li className='mb-6'>
+                  <div className='flex gap-5'>
+                    <Image className='block' src='/images/news-1.webp' width={100} height={80} alt='news'/>
+                    <div>
+                      <h5 className='text-xl font-bold hover:underline'>
+                        <a href="#">Aldi’s Holiday Beer and Wine Selection Has Landed..</a>
+                      </h5>
+                      <p className='text-gray-600'>12 mart 2024</p>
+                    </div>
+                  </div>
+                </li>
+                <li className='mb-6'>
+                  <div className='flex gap-5'>
+                    <Image className='block' src='/images/news-1.webp' width={100} height={80} alt='news'/>
+                    <div>
+                      <h5 className='text-xl font-bold hover:underline'>
+                        <a href="#">Aldi’s Holiday Beer and Wine Selection Has Landed..</a>
+                      </h5>
+                      <p className='text-gray-600'>12 mart 2024</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,35 +1,30 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
 
 type Props = {}
 
 function Navbar({ }: Props) {
+    const [mobileMenuOpen, setMobileMenu] = useState(false)
+    const [dropdownOpen, setDropdown] = useState(false);
     return (
         <nav className="bg-gray-100">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* <!-- Mobile menu button--> */}
-                        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <button onClick={() => setMobileMenu(!mobileMenuOpen)} type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                             <span className="absolute -inset-0.5"></span>
                             <span className="sr-only">Open main menu</span>
-                            {/* <!--
-                                Icon when menu is closed.
-
-                               Menu open: "hidden", Menu closed: "block"
-                            --> */}
-                            <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                            {/* Icon when menu is closed. Menu open: "hidden", Menu closed: "block" */}
+                            {!mobileMenuOpen && <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-                            {/* <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          --> */}
-                            <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                            </svg>}
+                            {/* Icon when menu is open. Menu open: "block", Menu closed: "hidden" */}
+                            {mobileMenuOpen && <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+                            </svg>}
                         </button>
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -38,15 +33,16 @@ function Navbar({ }: Props) {
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
-                                <Link href="/" className="rounded-md px-3 py-2 text-sm font-medium text-primary" aria-current="page">Home</Link>
-                                <Link href="/recipes" className="rounded-md px-3 py-2 text-sm font-medium text-gray-800  hover:text-primary">Recipes</Link>
-                                <Link href="/signup" className="rounded-md px-3 py-2 text-sm font-medium text-gray-800  hover:text-primary">Signup</Link>
-                                <Link href="/about" className="rounded-md px-3 py-2 text-sm font-medium text-gray-800  hover:text-primary">About</Link>
+                                <Link href="/" className="rounded-md px-3 py-2 text-md font-medium text-primary" aria-current="page">Home</Link>
+                                <Link href="/recipes" className="rounded-md px-3 py-2 text-md font-medium text-gray-800  hover:text-primary">Recipes</Link>
+                                <Link href="/signup" className="rounded-md px-3 py-2 text-md  font-medium text-gray-800  hover:text-primary">Signup</Link>
+                                <Link href="/blog" className="rounded-md px-3 py-2 text-md  font-medium text-gray-800  hover:text-primary">Blog</Link>
+                                <Link href="/about" className="rounded-md px-3 py-2 text-md font-medium text-gray-800  hover:text-primary">About</Link>
                             </div>
                         </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <button type="button" className="relative rounded-full p-1 text-gray-600 hover:text-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
                             <span className="absolute -inset-1.5"></span>
                             <span className="sr-only">View notifications</span>
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
@@ -56,44 +52,35 @@ function Navbar({ }: Props) {
 
                         {/* <!-- Profile dropdown --> */}
                         <div className="relative ml-3">
+                            {dropdownOpen && <button onMouseOver={() => setDropdown(false)} className="fixed top-0 left-0 bottom-0 right-0"></button>}
                             <div>
-                                <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                <button onClick={() => setDropdown(!dropdownOpen)} onMouseOver={() => setDropdown(true)} type="button" className="relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-primary" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                     <span className="absolute -inset-1.5"></span>
                                     <span className="sr-only">Open user menu</span>
                                     <Image className="h-8 w-8 rounded-full" height={50} width={50} src="/icons/icon-92x92.png" alt="avatar" />
                                 </button>
                             </div>
 
-                            {/* <!--
-            Dropdown menu, show/hide based on menu state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          --> */}
-                            <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
-                                {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a>
-                            </div>
+                            {dropdownOpen && <div className="absolute right-0 z-10 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
+                                <Link href="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</Link>
+                                <Link href="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</Link>
+                                <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-primary hover:text-white" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</Link>
+                            </div>}
                         </div>
                     </div>
                 </div>
             </div>
 
             {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-            <div className="sm:hidden" id="mobile-menu">
+            {mobileMenuOpen && <div className="sm:hidden" id="mobile-menu">
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     <Link href="/" className="block rounded-md bg-primary px-3 py-2 text-base font-medium text-white" aria-current="page">Home</Link>
                     <Link href="/recipes" className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:text-primary">Recipes</Link>
                     <Link href="/signup" className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:text-primary">Signup</Link>
+                    <Link href="/blog" className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:text-primary">Blog</Link>
                     <Link href="/about" className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:text-primary">About</Link>
                 </div>
-            </div>
+            </div>}
         </nav>
 
     )
