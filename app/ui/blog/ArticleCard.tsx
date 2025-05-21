7e9b763b6c4cd83b0bcdfdc2610b307ad2593c72
Salma-3 <salmehdhar@icloud.com>
2025-05-21 18:34:12 +0300

    fix article card to look all one size

--- app/ui/blog/ArticleCard.tsx
+++ app/ui/blog/ArticleCard.tsx
@@ -17,6 +17,8 @@ function ArticleCard({ article, editable, userId  }: Props) {
     const [deleteDisabled, setDisabled] = useState(false);
     const [visible, setVisible] = useState(true)
 
+    const { image, body, title } = article ;
+
     const onDelete = async () => {
         if(!window.confirm('Are you sure that you want to delete this?')) return;
         setDisabled(true)
@@ -28,32 +30,57 @@ function ArticleCard({ article, editable, userId  }: Props) {
         setDisabled(false)
     }
     return (
-        <div className={clsx("relative bg-white rounded-lg shadow-md overflow-hidden", !visible && 'hidden')}>
-            {article.image && <Image src={article.image.url} alt="Recipe Image" className="w-full h-48 object-cover" height={80} width={100}/>}
-            <div className="p-4">
-                <h3 className="text-xl text-gray-600 font-semibold mb-2">{article.title}</h3>
-                <p>{article.body.substring(0, 100).replaceAll('#', '').replaceAll('*', '')}</p>
-                {
-                    !editable ?
-                    <Link href={url} className="block text-center py-2 px-4 bg-primary text-white rounded-md hover:bg-orange-800">
-                       Read more
-                    </Link>
-                    : (
-                        <div className='flex gap-3 mt-4'>
-                            <Link href={`/blog/${article.slug}`} className="text-center py-2 px-4 bg-orange-500 text-white rounded-md hover:bg-orange-600">
-                               <span className='md:hidden'>View</span> <span className="ti ti-eye"></span>
-                            </Link>
-                            <Link href={url} className="text-center py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700">
-                            <span className='md:hidden'>Edit</span> <span className="ti ti-pencil-alt"></span>
-                            </Link>
-                            <button disabled={deleteDisabled} onClick={onDelete} className="text-center py-2 px-4 bg-red-600 text-white rounded-md hover:bg-red-700 disabled:bg-red-300">
-                            <span className='md:hidden'>Delete</span> <span className="ti ti-trash"></span>
-                            </button>
-                        </div>
-                    )
-                }
-            </div>
+       <div className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden h-[400px]">
+      {/* Header Image */}
+      <div className="relative w-full h-48">
+        {image && <Image
+          src={image.url}
+          alt="Card Header Image"
+          fill
+          className="object-cover"
+        />}
+      </div>
+
+      {/* Text Content */}
+      <div className="flex flex-col justify-between flex-1 p-4">
+        <div>
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
+          <p className="text-gray-600">{body.substring(0, 100).replaceAll('*', '').replaceAll('#', '').concat('...')}</p>
         </div>
+        <div className="mt-4">
+          { !editable ? <Link
+            href={url}
+            className="block text-center py-2 px-4 bg-primary text-white rounded-md hover:bg-orange-800"
+          >
+            Read more
+          </Link>  : (
+            <div className='flex w-full gap-3'>
+                <Link
+                    href={`/blog/${article.slug}`}
+                    className="text-center py-2 px-4 bg-orange-500 text-white rounded-md hover:bg-orange-600"
+                >
+                    <span className='md:hidden'>View</span> <span className="ti ti-eye"></span>
+                </Link>
+                <Link
+                    href={url}
+                    className="text-center py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700"
+                >
+                    <span className='md:hidden'>Edit</span> <span className="ti ti-pencil-alt"></span>
+                </Link>
+                <button
+                    disabled={deleteDisabled}
+                    onClick={onDelete}
+                    className="text-center py-2 px-4 bg-red-600 text-white rounded-md hover:bg-red-700 disabled:bg-red-300"
+                >
+                    <span className='md:hidden'>Delete</span> <span className="ti ti-trash"></span>
+                </button>
+            </div>
+          )
+         }
+        </div>
+
+      </div>
+    </div> 
     )
 }
 
